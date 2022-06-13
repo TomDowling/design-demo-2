8967c2b505523ce9dfce0bae275a25adb0444236
Tom Dowling <tom@digital-space.io>
2022-06-13 20:30:43 +0100

    Update: Styling the mobile header part 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+/*  Components  */
 import { Header } from "./Components";
 
+
+
+/*  RENDER  */
 export default function App() {
+    const [filters, setFilters] = useState("default");
+
     return (
         <div className="App">
             <Header />
--- src/Components/Layout/Header/index.js
+++ src/Components/Layout/Header/index.js
@@ -1,10 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+
+
+/*  RENDER  */
 export function Header() {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown((prevCheck) => !prevCheck)
+    }
+
     return (
         <header>
             <div className="container">
-                <div className="burger-menu" />
-                <div className="logo" />
+                <div
+                    className="burger-menu"
+                    onClick={toggleDropdown}
+                />
+
+                <div
+                    className="logo"
+                    onClick={() => alert("Home")}
+                />
+
+                <div
+                    className="notifications"
+                    onClick={() => alert("Notifications")}
+                />
             </div>
+
+            <ul className={`dropdown-menu ${showDropdown ? "" : "hide"}`}>
+                <li>Movies</li>
+                <li>Tv Shows</li>
+                <li>Directories</li>
+                <li className="spacer" />
+                <li>Search</li>
+                <li className="login">Login</li>
+            </ul>
         </header>
     )
 }
