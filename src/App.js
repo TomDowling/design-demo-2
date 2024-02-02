3132030879d9d6a944685061cc447d2ed6e445f5
Tom Dowling <tom@digital-space.io>
2024-02-02 12:14:15 +0000

    feat: adding codesandbox files

--- src/App.js
+++ src/App.js
@@ -8,45 +8,55 @@ import {
     Mobile,
     Responsive,
     Slider,
-    Tablet
+    Tablet,
 } from "./Components";
 
 /*  Constants  */
-import {
-    trendingNow
-} from "./Constants/Categories/trendingNow";
-
-
+import { trendingNow } from "./Constants/Categories/trendingNow";
 
 /*  RENDER  */
 export default function App() {
     const [filters, setFilters] = useState("default");
-    const [currentItem, setCurrentItem] = useState(
-        {
-            name: "Rampage",
-            image: "/assets/movies/rampage.jpg"
-        }
-    );
+    const [currentItem, setCurrentItem] = useState({
+        name: "Rampage",
+        image: "/assets/movies/rampage.jpg",
+    });
 
     return (
         <div className="App">
             <Header setFilters={setFilters} />
             <HighlightedItem currentItem={currentItem} />
-            
+
             {filters !== "tv" && (
-                <Slider title="Trending Movies" items={trendingNow} setCurrentItem={setCurrentItem} />
+                <Slider
+                    title="Trending Movies"
+                    items={trendingNow}
+                    setCurrentItem={setCurrentItem}
+                />
             )}
 
             {filters !== "movies" && (
-                <Slider title="Trending Tv Shows" items={trendingNow} setCurrentItem={setCurrentItem} />
+                <Slider
+                    title="Trending Tv Shows"
+                    items={trendingNow}
+                    setCurrentItem={setCurrentItem}
+                />
             )}
 
             {filters === "default" && (
-                <Slider title="New Releases" items={trendingNow} setCurrentItem={setCurrentItem} />
+                <Slider
+                    title="New Releases"
+                    items={trendingNow}
+                    setCurrentItem={setCurrentItem}
+                />
             )}
 
             {filters !== "tv" && (
-                <Slider title="Action Movies" items={trendingNow} setCurrentItem={setCurrentItem} />
+                <Slider
+                    title="Action Movies"
+                    items={trendingNow}
+                    setCurrentItem={setCurrentItem}
+                />
             )}
         </div>
     );
